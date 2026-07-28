b66854c1e8551148b763ee6a3b17a4635fdb40f8
jsperez1191 <jsperez1191@gmail.com>
2026-07-28 07:07:31 -0700

    Add files via upload

---
+++ stats.sql
@@ -0,0 +1,19 @@
+create table stats.stats
+(
+    last_first         text   null,
+    player_id          int    null,
+    year               int    null,
+    pa                 int    null,
+    k_percent          double null,
+    bb_percent         double null,
+    woba               double null,
+    xwoba              double null,
+    sweet_spot_percent double null,
+    barrel_batted_rate double null,
+    hard_hit_percent   double null,
+    avg_best_speed     double null,
+    avg_hyper_speed    double null,
+    whiff_percent      double null,
+    swing_percent      double null
+);
+
